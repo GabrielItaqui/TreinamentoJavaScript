b476cf280c1be5fab70e70cba5c104d6d5217a52
GabrielItaqui <itaquips4@gmail.com>
2022-05-23 16:07:03 -0300

    Adição da criação de curso.

--- Admin.js
+++ Admin.js
@@ -6,10 +6,12 @@ class Admin extends User {
         //função superclasse (está acima na cadeia, sub classes estariam abaixo)
         super(nome, email, nascimento, role, ativo)
     }
+    //Dentro de classes não é necessário usar a terminologia function
+    criarCurso (nomeDoCurso, vagas) {
+        return `Curso de ${nomeDoCurso} criado com ${vagas} vagas.`
+    }
 }
 
 const novoAdmin = new Admin ('Rodrigo', 'r@r.com', '2021-01-01');
 
-console.log(novoAdmin);
-console.log(novoAdmin.exibirInfos());
-console.log(novoAdmin);
+console.log(novoAdmin.criarCurso("JS", "20"))
